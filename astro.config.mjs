53f38ab6934d9f67acc79acd408c4e04d84f4766
Vladyslav <vlad@turn.lv>
2025-03-19 11:30:12 +0200

    init commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,9 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: "https://wall2.turn.lv/",
+  integrations: [sitemap()],
+});
